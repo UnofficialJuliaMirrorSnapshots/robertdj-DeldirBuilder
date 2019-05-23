8b2de6da06b72a7ed0e841eab5c5840669e23bba
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-23 15:35:02 +0000

    Snapshot of branch master taken on 2019-05-23T11:35:00.842-04:00 from "https://github.com/UnofficialJuliaMirror/robertdj-DeldirBuilder"

---
+++ README.md
@@ -0,0 +1,9 @@
+# DeldirBuilder
+
+[![Build Status](https://travis-ci.org/robertdj/DeldirBuilder.svg?branch=master)](https://travis-ci.org/robertdj/DeldirBuilder)
+
+This repository builds binary artifacts for the Deldir project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/robertdj/DeldirBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,63 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "DeldirBuilder"
+version = v"0.1.9"
+
+# Collection of sources required to build DeldirBuilder
+sources = [
+    "https://cran.microsoft.com/snapshot/2016-02-01/src/contrib/deldir_0.1-9.tar.gz" =>
+    "090aba6747efc77424a44bf4aaa229dfc01fff7752720689bb70cd861df61f6a",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/deldir/src
+
+for f in *.f; do
+    ${FC} -fPIC -O2 -pipe -g -c "${f}" -o "$(basename "${f}" .f).o"
+done
+
+if [[ ${target} == *-mingw32 ]]; then
+    libdir="bin"
+else
+    libdir="lib"
+fi
+
+mkdir -p "${prefix}/${libdir}"
+${CC} -shared -o ${prefix}/${libdir}/libdeldir.${dlext} *.o
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    MacOS(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libdeldir", :libdeldir)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
